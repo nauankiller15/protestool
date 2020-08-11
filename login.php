3f98c6695e1048efd170667554cc232f205b9738
nauankiller15 <nauan.alcantara1@gmail.com>
2020-08-11 16:59:13 -0300

    Correções de erro de algumas funções das páginas e acrescentei uma animação inicial na login.php

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -7,28 +8,32 @@
     <link rel="stylesheet" type="text/css" href="stylelogin.css">
     <title>Entrar</title>
 </head>
+
 <body>
-    
-<div class="titulo">
-    <h2>ProtesTool</h2>
-</div>
-<div class="header">
+
+    <div class="titulo">
+        <h2>ProtesTool</h2>
+    </div>
+    <div class="fadeIn">
+    <div class="header">
     </div>
 
-    <form method="post" action="login.php">
-        <div class="input-group">
-            <label>Usuário:</label>
-            <input type="text" placeholder="Digite aqui o usuário de administrador" name="username" required>
-        </div>
-        <div class="input-group">
-            <label>Senha:</label>
-            <input type="password" placeholder="Digite a senha de administrador" name="password" required>
-        </div>
+        <form method="post" action="login.php">
+            <div class="input-group">
+                <label>Usuário:</label>
+                <input type="text" placeholder="Digite aqui o usuário de administrador" name="username" required>
+            </div>
+            <div class="input-group">
+                <label>Senha:</label>
+                <input type="password" placeholder="Digite a senha de administrador" name="password" required>
+            </div>
 
-        <div class="input-group">
-            <button type="Submit" name="login" class="btn">Entrar</button>
-        </div>
-        <p>Acesso somente autorizado!</p>
-    </form>
+            <div class="input-group">
+                <button type="Submit" name="login" class="btn">Entrar</button>
+            </div>
+            <p>Acesso somente autorizado!</p>
+        </form>
+    </div>
 </body>
+
 </html>
