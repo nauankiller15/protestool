a00b111b6867ffe79b93cb848672276ed3896707
nauankiller15 <nauan.alcantara1@gmail.com>
2020-08-18 22:30:03 -0300

    Exclusão de um script html5, que estava dando erro

--- includes/headindex.php
+++ includes/headindex.php
@@ -1,7 +1,6 @@
 
 <head>
     <meta charset="UTF-8">
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="CSSs/styleindex.css">
     <title>Protestool</title>
--- includes/headlogin.php
+++ includes/headlogin.php
@@ -1,6 +1,5 @@
 <head>
     <meta charset="UTF-8">
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="CSSs/stylelogin.css">
     <title>Protestool - Entrar</title>
