4173896c73604c0e8a8a5b5807bf41a5958ba9d9
nauankiller15 <nauan.alcantara1@gmail.com>
2020-08-26 19:32:27 -0300

    alterações de z-index do menu tools

--- includes/menu.php
+++ includes/menu.php
@@ -4,16 +4,16 @@
         <li class="menu"><a href="http://localhost/ProtesTool/consulta_daje.php">Consultar Daje</a></li>
         <li class="menu"><a href="http://localhost/ProtesTool/reenviar_daje.php">Reenviar Daje</a></li>
         <div class="tools">
-            <span data-tooltip2="Sair do sistema"><a href="#sair"><button type="sair" class="btnsair">✖️</button></a></span>
+            <span data-tooltip4="Sair do sistema"><a href="#sair"><button type="sair" class="btnsair">✖️</button></a></span>
             <div class="circleoff" style="animation-delay: 0s"></div>
             <div class="circleoff" style="animation-delay: 0.8s"></div>
             <div class="circleoff" style="animation-delay: 1.8s"></div>
             <div class="circleon" style="animation-delay: 0s"></div>
             <div class="circleon" style="animation-delay: 0.8s"></div>
             <div class="circleon" style="animation-delay: 1.8s"></div>
-            <span data-tooltip2="Parametrizar configurações">
+            <span data-tooltip2="Parâmetros do servidor">
                 <div class="tooltip">
-                    <span class="tooltiptext">Parametrizar Configurações.
+                    <span class="tooltiptext">Parametrizar configurações: ⚙️
                 </div><a href="javascript: abrir();"><button class="gear">⚙️</button></a>
             </span>
             </span>
--- js/ajax.js
+++ js/ajax.js
@@ -83,6 +83,7 @@ $(document).ready(function() {
     $('#reset').on('click', function() {
         $(".circleoff").show();
         $(".circleon").hide();
+        $(".tooltip,.tooltiptext").show();
     });
 });
 
