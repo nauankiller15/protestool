e38ba86e693564ff2f75c54d88ad53a4c1ef4053
nauankiller15 <nauan.alcantara1@gmail.com>
2020-08-26 14:34:14 -0300

    Notificação ajustada para trás do menu

--- includes/menu.php
+++ includes/menu.php
@@ -4,7 +4,7 @@
         <li><a href="http://localhost/ProtesTool/consulta_daje.php">Consultar Daje</a></li>
         <li><a href="http://localhost/ProtesTool/reenviar_daje.php">Reenviar Daje</a></li>
             <div class="tools">
-                <span data-tooltip2="Sair do sistema"><button type="sair" class="btnsair">✖️</button></span>
+                <span data-tooltip2="Sair do sistema"><a href= "#sair"><button type="sair" class="btnsair">✖️</button></a></span>
                 <div class="circle" style="animation-delay: 0s"></div>
                 <div class="circle" style="animation-delay: 0.8s"></div>
                 <div class="circle" style="animation-delay: 1.8s"></div>
