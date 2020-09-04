9f53e8865b19e582a87cb7d8424a6f222ee1779b
nauankiller15 <nauan.alcantara1@gmail.com>
2020-09-04 16:11:56 -0300

    regra na div de consulta daje

--- desvincular_daje_page.php
+++ desvincular_daje_page.php
@@ -8,7 +8,7 @@
 <form class="api-request" data-endpoint="DesvincularApontamento">
     <div class="input-group">
         <label>Número do Daje:</label>
-        <input type="text" placeholder="Digite aqui o número do DAJE sem espaços" name="numero_daje" required>
+        <input pattern="[0-9]{4}[A-Z]{2}[0-9]{6}" type="text" placeholder="Digite aqui o número do DAJE sem espaços" name="numero_daje" required>
         <p><b>Emissor, série e número</b> do <b>DAJE</b> necessários.</p>
     </div>
 
