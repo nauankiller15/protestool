b724ba602367848fd8d7f05b91d9aa517b1b45a9
nauankiller15 <nauan.alcantara1@gmail.com>
2020-09-08 22:57:55 -0300

    novo input na tela de configuração

--- includes/footer.php
+++ includes/footer.php
@@ -7,15 +7,14 @@
         </div>
         <span data-tooltip3="Fechar tela"><a href="javascript: fechar();"><button type="submit" class="btnsair">✖️</button></a></span>
         <form class="api-request" data-endpoint="Config">
-            <div class="input-group2">
-                <span>
-                    <label>IP do Servidor:</label>
-                    <input id="ip" pattern="[0-9]{3}.[0-9]{3}.[0-9]{2}.[0-9]{3}" class ="ip1" type="text" placeholder="Ex.:192.168.XXX.XXX" name="ip" required>
-                    <p>Dica: Por padrão utiliza-se: <b>192.168.15.15/150</b>.</p>
-                </span>
+            <div class="wrap-input100 validate-input m-b-23">
+                <span class="label-input100">IP do servidor:</span>
+                <input class="input100" id="ip" pattern="[0-9]{3}.[0-9]{3}.[0-9]{2}.[0-9]{3}" class="ip" type="text" placeholder="Ex.:192.168.XXX.XXX" name="ip" required>
+                <span class="focus-input100" data-symbol="&#xf233;"></span>
             </div>
+              <p class="input-group2">Dica: Por padrão utiliza-se: <b>192.168.15.15/150</b>.</p>
             <div class="centralizar3">
-               <button type="save" id="save" class="btn3">Salvar</button> <button id="reset" type="reset" class="btnlimpar">Limpar</button>
+                <button type="save" id="save" class="btn3">Salvar</button> <button id="reset" type="reset" class="btnlimpar">Limpar</button>
             </div>
         </form>
     </div>
