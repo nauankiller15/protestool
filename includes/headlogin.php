d03b02793f3d63fe0e5eff0afc26f9c3d2afe442
nauankiller15 <nauan.alcantara1@gmail.com>
2020-09-18 00:14:21 -0300

    alterações na página de login

--- includes/headlogin.php
+++ includes/headlogin.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="CSSs/stylelogin.css">
     <link rel="stylesheet" type="text/css" href="CSSs/util.css">
+    <link rel="stylesheet" type="text/css" href="CSSs/styles.css">
     <link rel="stylesheet" type="text/css" href="CSSs/login-form.css">
     <link rel="stylesheet" type="text/css" href="fonts/font-awesome-5.14.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
